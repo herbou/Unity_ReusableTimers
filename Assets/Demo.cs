0330f8e3f0c6c69fb6ec621a91808445a2c4fcb8
Hamza HERBOU <hamza95herbou@gmail.com>
2020-12-23 18:17:44 +0100

    added files

---
+++ Assets/Demo.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine ;
+
+public class Demo : MonoBehaviour {
+
+   [SerializeField] Timer timer1 ;
+   [SerializeField] Timer timer2 ;
+   [SerializeField] Timer timer3 ;
+   [SerializeField] Timer timer4 ;
+
+   private void Start () {
+      timer1
+      .SetDuration (6)
+      .OnEnd (() => Debug.Log ("Timer 1 ended"))
+      .Begin () ;
+
+      timer2
+      .SetDuration (10)
+      .OnEnd (() => Debug.Log ("Timer 2 ended"))
+      .Begin () ;
+
+      timer3
+      .SetDuration (15)
+      .OnEnd (() => Debug.Log ("Timer 3 ended"))
+      .Begin () ;
+
+      timer4
+      .SetDuration (25)
+      .OnEnd (() => Debug.Log ("Timer 4 ended"))
+      .Begin () ;
+   }
+
+}
---
+++ Assets/Timers/Timer.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine ;
+using UnityEngine.UI ;
+using UnityEngine.Events ;
+using System.Collections ;
+
+public class Timer : MonoBehaviour {
+   [Header ("Timer UI references :")]
+   [SerializeField] private Image uiFillImage ;
+   [SerializeField] private Text uiText ;
+
+   public int Duration { get; private set; }
+
+   public bool IsPaused { get; private set; }
+
+   private int remainingDuration ;
+
+   // Events --
+   private UnityAction onTimerBeginAction ;
+   private UnityAction<int> onTimerChangeAction ;
+   private UnityAction onTimerEndAction ;
+   private UnityAction<bool> onTimerPauseAction ;
+
+   private void Awake () {
+      ResetTimer () ;
+   }
+
+   private void ResetTimer () {
+      uiText.text = "00:00" ;
+      uiFillImage.fillAmount = 0f ;
+
+      Duration = remainingDuration = 0 ;
+
+      onTimerBeginAction = null ;
+      onTimerChangeAction = null ;
+      onTimerEndAction = null ;
+      onTimerPauseAction = null ;
+
+      IsPaused = false ;
+   }
+
+   public void SetPaused (bool paused) {
+      IsPaused = paused ;
+
+      if (onTimerPauseAction != null)
+         onTimerPauseAction.Invoke (IsPaused) ;
+   }
+
+
+   public Timer SetDuration (int seconds) {
+      Duration = remainingDuration = seconds ;
+      return this ;
+   }
+
+   //-- Events ----------------------------------
+   public Timer OnBegin (UnityAction action) {
+      onTimerBeginAction = action ;
+      return this ;
+   }
+
+   public Timer OnChange (UnityAction<int> action) {
+      onTimerChangeAction = action ;
+      return this ;
+   }
+
+   public Timer OnEnd (UnityAction action) {
+      onTimerEndAction = action ;
+      return this ;
+   }
+
+   public Timer OnPause (UnityAction<bool> action) {
+      onTimerPauseAction = action ;
+      return this ;
+   }
+
+
+
+
+
+   public void Begin () {
+      if (onTimerBeginAction != null)
+         onTimerBeginAction.Invoke () ;
+
+      StopAllCoroutines () ;
+      StartCoroutine (UpdateTimer ()) ;
+   }
+
+   private IEnumerator UpdateTimer () {
+      while (remainingDuration > 0) {
+         if (!IsPaused) {
+            if (onTimerChangeAction != null)
+               onTimerChangeAction.Invoke (remainingDuration) ;
+
+            UpdateUI (remainingDuration) ;
+            remainingDuration-- ;
+         }
+         yield return new WaitForSeconds (1f) ;
+      }
+      End () ;
+   }
+
+   private void UpdateUI (int seconds) {
+      uiText.text = string.Format ("{0:D2}:{1:D2}", seconds / 60, seconds % 60) ;
+      uiFillImage.fillAmount = Mathf.InverseLerp (0, Duration, seconds) ;
+   }
+
+   public void End () {
+      if (onTimerEndAction != null)
+         onTimerEndAction.Invoke () ;
+
+      ResetTimer () ;
+   }
+
+
+   private void OnDestroy () {
+      StopAllCoroutines () ;
+   }
+}
